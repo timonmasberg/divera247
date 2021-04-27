8ed4ea3b472b9f069b14a3ce94b8dd44db7dc9ef
Timon Masberg <kontakt@timonmasberg.de>
2021-04-27 02:11:14 +0200

    Add abstract mapper class that handles mapping from API results into internal types

---
+++ src/mapper.ts
@@ -0,0 +1,17 @@
+import {sortObjectsByKeyRanking} from "./helpers/sort.helper";
+
+export abstract class Mapper<ApiResultType, MappedType> {
+  protected _data: ApiResultType;
+
+  protected constructor(data: ApiResultType) {
+    this._data = data;
+
+  }
+
+  sort(keyRanking: string[]): Mapper<ApiResultType, MappedType> {
+    this._data = sortObjectsByKeyRanking(this._data, keyRanking);
+    return this;
+  }
+
+  abstract getAsMapped(): MappedType;
+}
