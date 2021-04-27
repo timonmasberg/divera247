296600b44e7eb9a996ed643b748c796298cbcf65
Timon Masberg <kontakt@timonmasberg.de>
2021-04-27 02:11:57 +0200

    Add group mapper that maps api result into group model

---
+++ src/endpoints/pull/mapper/groups.mapper.ts
@@ -0,0 +1,30 @@
+import {Mapper} from "../../../mapper";
+import {Group} from "../models/group.model";
+import {GroupApiResult} from "../models/group-api-return.model";
+
+export class GroupsMapper extends Mapper<GroupApiResult, Group[]> {
+  constructor(data: GroupApiResult) {
+    super(data);
+  }
+
+  getAsMapped(): Group[] {
+    const groups: Group[] = [];
+
+    Object.entries(this._data).forEach(entry => {
+      const [groupId, data] = entry;
+      const group = GroupsMapper.factorGroupFromEntry(groupId, data);
+
+      groups.push(group);
+    });
+
+    return groups;
+  }
+
+  private static factorGroupFromEntry(id: string, data: any): Group {
+    return {
+      id,
+      name: data.name,
+      ric: data.ric
+    }
+  }
+}
