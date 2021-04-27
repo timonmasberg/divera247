ff949de944b281e8b05e314bf509012210709933
Timon Masberg <kontakt@timonmasberg.de>
2021-04-27 02:15:52 +0200

    Add PullAll class that represents /pull/all/ endpoint Add get groups functionality that extracts groups and models them as internal group type Add get by path functionality that extracts a specific value

---
+++ src/endpoints/pull/pull-all.ts
@@ -0,0 +1,35 @@
+import {Group} from "./models/group.model";
+import {DiveraEndpoint} from "../../divera-endpoint";
+import {getPropertyValueByPath} from "../../helpers/get-property-value-by-path.helper";
+import {GroupsMapper} from "./mapper/groups.mapper";
+import {GroupApiResult} from "./models/group-api-return.model";
+
+export class PullAll extends DiveraEndpoint {
+  constructor(accessKey: string) {
+    super(accessKey, 'pull/all/');
+  }
+
+  getGroups(returnSorted = false): Promise<Group[]> {
+    return this.get()
+      .then(data => {
+        const group = getPropertyValueByPath<any, GroupApiResult>(data, "cluster", "group");
+
+        const mapper = new GroupsMapper(group);
+
+        if (returnSorted) {
+          const idRanking = getPropertyValueByPath<any, number[]>(data, "cluster", "groupsorting");
+          mapper.sort(idRanking.map(String))
+        }
+
+        return mapper.getAsMapped();
+      })
+  }
+
+
+  getByPath<ReturnType>(...keys: string[]): Promise<ReturnType> {
+    return this.get().then(data => getPropertyValueByPath<any, ReturnType>(data, ...keys));
+  }
+}
+
+
+
