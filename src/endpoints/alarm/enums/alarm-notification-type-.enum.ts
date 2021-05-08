48bb088dc0c65ce52ef0ddf13e0693fb2899182f
Timon Masberg <kontakt@timonmasberg.de>
2021-05-08 21:55:13 +0200

    Remove deprecated property from alarm model Add alarm notification type enum that represents the notification type of an alarm

---
+++ src/endpoints/alarm/enums/alarm-notification-type-.enum.ts
@@ -0,0 +1,6 @@
+export enum AlarmNotificationType {
+  SELECTED_LOCATIONS = 1,
+  ALL_OF_LOCATION = 2,
+  SELECTED_GROUPS = 3,
+  SELECTED_USERS = 4
+}
--- src/endpoints/alarm/models/create-alarm.model.ts
+++ src/endpoints/alarm/models/create-alarm.model.ts
@@ -1,54 +1,48 @@
+export declare type MappedValue = string | number;
+
+
 export interface CreateAlarm {
-  "Alarm": {
-    "foreign_id": string,
-    "alarmcode_id": number,
-    "priority": true,
-    "title": string,
-    "text": string,
-    "address": string
-    "lat": number,
-    "lng": number,
-    "report": string,
-    "private_mode": boolean,
-    "notification_type": number,
-    "notification_filter_vehicle": boolean,
-    "notification_filter_status": boolean,
-    "notification_filter_access": boolean,
-    "send_push": boolean,
-    "send_sms": boolean,
-    "send_call": boolean,
-    "send_mail": boolean,
-    "send_pager": boolean,
-    "response_time": number,
-    "closed": boolean,
-    "ts_close": number,
-    "ts_publish": number,
-    "notification_filter_status_access": boolean,
-    "long": number,
-    "type": string,
-    "uploads": number,
-    "clusters": {
-      [id: string]: {
-        "notification_type": number
-      },
-    },
-    "group": number[],
-    "user_cluster_relation": number[],
-    "vehicle": number[],
-    "status": number[]
-  },
-  "instructions": {
-    "cluster": {
-      "mapping": string
-    },
-    "group": {
-      "mapping": string
-    },
-    "user_cluster_relation": {
-      "mapping": string
-    },
-    "vehicle": {
-      "mapping": string
-    }
-  }
+  Alarm: {
+    foreign_id: string;
+    alarmcode_id: number;
+    priority: boolean;
+    title: string;
+    text: string;
+    address: string;
+    lat: number;
+    lng: number;
+    report: string;
+    private_mode: boolean;
+    notification_type: number;
+    notification_filter_vehicle: boolean;
+    notification_filter_status: boolean;
+    notification_filter_access: boolean;
+    send_push: boolean;
+    send_sms: boolean;
+    send_call: boolean;
+    send_mail: boolean;
+    send_pager: boolean;
+    response_time: number;
+    closed: boolean;
+    ts_close: number;
+    ts_publish: number;
+    long: number;
+    type: string;
+    uploads: number;
+    clusters: {
+      [p: string]: {
+        notification_type: number
+      }
+    };
+    group: MappedValue[];
+    user_cluster_relation: MappedValue[];
+    vehicle: MappedValue[];
+    status: MappedValue[]
+  };
+  instructions: {
+    cluster: { mapping: string };
+    group: { mapping: string };
+    user_cluster_relation: { mapping: string };
+    vehicle: { mapping: string }
+  };
 }
