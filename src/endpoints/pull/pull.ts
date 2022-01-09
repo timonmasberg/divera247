2df1edf931e972ec7dc26a0fa5c9c627a2a311b9
Hendrik Abbenhaus <kiyon1995@gmail.com>
2022-01-09 21:03:12 +0100

    Add vehicle-status-pull

--- src/endpoints/pull/pull.ts
+++ src/endpoints/pull/pull.ts
@@ -3,6 +3,8 @@ import {BaseClient} from "../../base-client";
 import {getPropertyValueByPath} from "../../helpers/get-property-value-by-path.helper";
 import {GroupsMapper} from "./mapper/groups.mapper";
 import {GroupApiResult, GroupCluster, Groups} from "./models/group-api-result.model";
+import {DiveraResponse} from "../divera-response.model";
+import {Vehicle} from "./models/vehicle.model";
 
 export class Pull extends BaseClient {
   getGroups(returnSorted = false): Promise<Group[]> {
@@ -26,6 +28,11 @@ export class Pull extends BaseClient {
     return this.get<{ data: any }>("pull/all/")
       .then(res => res.data)
       .then(data => getPropertyValueByPath<any, ReturnType>(data, ...keys));
+  }
+
+  getVehicleStatus(): Promise<Vehicle[]>{
+    return this.get<DiveraResponse<Vehicle[]>>("v2/pull/vehicle-status")
+      .then(resp => resp.data)
   }
 }
 
